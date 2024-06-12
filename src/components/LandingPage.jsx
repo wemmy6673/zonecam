6d117c8794015a7abb3cc8980b85c9221299e86a
wemmy6673 <okunolawemzy@gmail.com>
2024-06-12 18:00:00 +0100

    edit

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,7 +8,7 @@ const LandingPage = () => {
         <div className="mx-auto justify-center">
             <div className='flex flex-col px-16 md:px-0 space-y-2 items-center w-[100%] pt-16'>
                 <img src={Logo} width="300" height="300" className=""/>
-                <div className='flex flex-col space-y-72 md:space-y-4'>
+                <div className='flex flex-col space-y-8 md:space-y-4'>
                     <p className='max-w-sm text-justify'>
                         Discover your roots with ZoneCam, the cutting-edge app that leverages advanced facial recognition technology to predict the geopolitical zone of origin for Nigerians. 
 

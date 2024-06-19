1b90432f22a25f8630f52342d4a716cc6b528b47
wemmy6673 <okunolawemzy@gmail.com>
2024-06-19 21:26:49 +0100

    camera testing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,44 @@
 import { BsCamera } from "react-icons/bs";
 import { BsCapslock } from "react-icons/bs";
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 
 const Home = () => {
 
+    const [image, setImage] = useState(null);
+    const [isCameraOpen, setIsCameraOpen] = useState(false);
+    const [imageSrc, setImageSrc] = useState(null);
     const videoRef = useRef(null);
-    // const canvasRef = useRef(null);
+    const canvasRef = useRef(null);
 
+    const handleImageCapture = (e) =>{
+      const file = e.target.files[0];
+      if(file) {
+        const imageUrl = URL.createObjectUrl(file);
+        setImage(imageUrl);
+      }
+    };
 
+    const openCamera = async () => {
 
-    const startCamera = async () => {
-        try {
-          const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-          videoRef.current.srcObject = stream;
-          videoRef.current.play();
-        } catch (error) {
-          console.error('Error accessing camera:', error);
-        }
-      };
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({video: true});
+        videoRef.current.srcObject = stream;
+        setIsCameraOpen(true);
+      }
+      catch (err) {
+        console.error('Error accessing media devices.', err);
+      }
+    };
 
-    //   const stopCamera = () => {
-    //     const stream = videoRef.current.srcObject;
-    //     const tracks = stream.getTracks();
+    const takePicture = () =>{
+      const context = canvasRef.current.getContext('2d');
+      context.drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height);
+      setImageSrc(canvasRef.current.toDataURL('image/png'));
+
+    }
+
     
-    //     tracks.forEach((track) => {
-    //       track.stop();
-    //     });
-    
-    //     videoRef.current.srcObject = null;
-    //   };
+
     
 
     return(
@@ -36,23 +46,50 @@ const Home = () => {
             <div className='flex flex-col space-y-10 w-[100%] items-center'>
             <h1 className='text-green-600 text-2xl font-bold'>WELCOME!</h1>
 
-            <div className='flex flex-col space-y-8 md:space-y-0 md:flex-row md:space-x-6 cursor-pointer'>
-                <div className='border border-green-600 text-green-600  p-10 rounded-2xl items-center' onClick={startCamera()}>
+            <div className='flex flex-col space-y-8 md:space-y-0 md:flex-row md:space-x-6 cursor-pointer' onClick={openCamera}>
+              { !isCameraOpen ? (
+                <div className='border border-green-600 text-green-600  p-10 rounded-2xl items-center'>
 
 
                 <BsCamera className='text-6xl' />
 
                 <p className='py-2'>Take Photo</p>
+
                 
 
-                </div>
+                </div>) : (
 
+                   <>
+                <video ref={videoRef} autoPlay style={{ width: '300px', height: 'auto' }}></video>
+                <button onClick={takePicture}>Capture Image</button>
+                <canvas ref={canvasRef} style={{}} width="300" height="200"></canvas>
+                  {imageSrc && (
+                    <div>
+                       <h2>Captured Image:</h2>
+                        <img src={imageSrc} alt="Captured" style={{ width: '300px', height: 'auto' }} />
+                   </div>
+                )}
+        </>
+
+
+                )
+
+                }
+
+                
                 <div className='border border-green-600 text-green-600  p-10 rounded-2xl items-center'>
 
 
                 <BsCapslock className='text-6xl' />
 
                 <p className='py-2'>Upload Photo</p>
+
+                <input className='text-black' type='file' accept="image/*" capture='environment' id='cameraInput' onChange={handleImageCapture} />
+                {image && (
+                  <div>
+                    <img src={image} alt="captured" style={{width: '300px', height: 'auto'}}/>
+                  </div>
+                )}
 
                 </div>
 
